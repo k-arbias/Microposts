4f51f8bc1a645122586bba9e4b576329e4d5e9ad
Arbias <52889109+k-arbias@users.noreply.github.com>
2019-12-09 15:42:49 +0100

    Edit state

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 //Listen for remove
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+//Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 function getPosts(){
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
@@ -44,5 +47,24 @@ function submitPost(){
         .catch(err => console.log(err));
       }
     }
+  e.preventDefault();
+}
+
+function enableEdit(e){
+
+  if(e.target.parentElement.classList.contains('edit')){
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    
+    const data = {
+      id,
+      title,
+      body
+    }
+    //Fill the form with current post
+    ui.fillForm(data);
+  }
+
   e.preventDefault();
 }
--- src/ui.js
+++ src/ui.js
@@ -58,6 +58,11 @@ class UI {
         this.titleInput.value = '';
         this.bodyInput.value = '';
     }
+    fillForm(data){
+        this.titleInput.value = data.title;
+        this.bodyInput.value = data.body;
+        this.idInput.value = data.id;
+    }
 }
 
 export const ui = new UI();
